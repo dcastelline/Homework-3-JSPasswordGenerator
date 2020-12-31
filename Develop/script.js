14c94b0b253f1b01425b0a3654115b08c33dc13a
Daniel <daniel.castelline@gmail.com>
2020-12-31 09:24:46 -0600

    committing original js to undo everything

--- Develop/script.js
+++ Develop/script.js
@@ -10,45 +10,16 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// My attempt
-
-// Prompts to store user input
-var passLength = prompt("Password Length: Choose from 8 to 128 characters");
-  passLength = passLength[math.floor(math.random() * 128) + 7];
-
-var confirmLCase = confirm("Do you want to include lowercase letters?");
-
-var confirmUCase = confirm("Do you want to include uppercase letters?");
-
-var confirmNum = confirm("Do you want to include numbers?");
- 
-var confirmSpcl = confirm("Do you want to include special characters?");
- 
-// Confirm selections
-var confChoices = confirm("Are these selections correct?");
-
-// Password generated alert
-alert("Your password is: " + password);
-
-// Password generator function attempts
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-
-  
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Password generated alert
+alert("Your password is: " + password);
