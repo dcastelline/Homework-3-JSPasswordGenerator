7e1fd265daab6ece74292821276ee3bbc306a03f
Daniel <daniel.castelline@gmail.com>
2020-12-31 09:57:42 -0600

    added arrays and variables needed for function

--- Develop/script.js
+++ Develop/script.js
@@ -9,9 +9,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variable arrays
+lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+numbers = [0,1,2,3,4,5,6,7,8,9];
+symbols = ["!","@","#","$","%","^","&","*","(",")"];
+
+// Variables for characters included in password
+var characterAmount = characterAmountNumber.value
+var includeLowercase = includeLowercase.value
+var includeUppercase = includeUppercase.value
+var includeNumbers = includeNumbers.value
+var includeSymbols = includeSymbols.value
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols, includeLowercase);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
