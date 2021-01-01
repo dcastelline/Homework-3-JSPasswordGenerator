a23c0f5e3540b095cf34d91c1af0297eb0a5e64e
Daniel <daniel.castelline@gmail.com>
2020-12-31 20:15:07 -0600

    deleted some notes

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,3 @@
-
-// Click button to generate button
-// Prompts for password criteria
-// Prompt for length between 8 and 128 characters
-// Choices for lowercase, uppercase, numbers, and/or special characters
-// Input validated for each selection
-// After prompts, password generated and displayed in alert or written to page
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
